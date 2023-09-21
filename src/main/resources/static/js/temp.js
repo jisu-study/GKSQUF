003142f63556794bfe8481d96f9948ceb1f4db47
LeeHanByeol <byol0425@gmail.com>
2023-09-22 02:42:09 +0900

    수정 전

---
+++ src/main/resources/static/js/temp.js
@@ -0,0 +1,100 @@
+var main = {
+
+    init: function (){
+        var _this = this;
+
+        $("#btn-upload").click(function (){
+            _this.save();
+        });
+
+        $("#btn-delete").click(function (){
+            _this.delete();
+        });
+    },
+
+    loadTable: function (){
+        const displayList = document.getElementById('display-list');
+        const totalElement = document.getElementById('total');
+        let total = 0;
+
+        displayList.innerHTML = '';
+    },
+
+    save: function (){
+        var data = {
+            costCategory: $("#category").val(),
+            costDetails: $("#details").val(),
+            costAmount: $("#amount").val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: '/api/v1/recordCosts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data),
+            success: function (response){
+                alert('OK');
+            },
+            error: function (xhr, status, error){
+                console.log(xhr);
+                alert('fail');
+            }
+        });
+
+//        var total = $("#total").val();
+        //update total
+//        total.text(total + data.amount);
+    },
+
+    update: function (){
+        var data = {
+            details: $("#details").val(),
+            amount: $("#amount").val()
+        };
+
+        var costId = $('#cost-id').val();
+
+        $.ajax({
+            type: "PUT",
+            url: 'api/v1/recordCosts/'+costId,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data),
+            success: function (response){
+                alert('OK');
+            },
+            error: function (xhr, status, error){
+                alert('fail');
+            }
+        });
+
+        var total = $("#total").val();
+        //update total
+        total.text(total - 0 + data.amount);
+    },
+
+    delete: function (){
+        var costId = $('#cost-id').val();
+        var amount = $('#amount').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: 'api/v1/recordCosts/'+costId,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            success: function (response){
+                alert('OK');
+            },
+            error: function (xhr, status, error){
+                alert('fail');
+            }
+        })
+
+        var total = $("#total").val();
+        //update total
+        total.text(total - amount);
+    }
+};
+
+main.init();
